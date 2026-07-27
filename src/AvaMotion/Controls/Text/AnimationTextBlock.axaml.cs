8d57005f0e3146adcbc21208c15311fb283c614c
Dime <liy_0924@qq.com>
2026-07-27 12:45:23 +0800

    feat: init project

---
+++ src/AvaMotion/Controls/Text/AnimationTextBlock.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaMotion.Controls.Text;
+
+public partial class AnimationTextBlock : UserControl
+{
+    public AnimationTextBlock()
+    {
+        InitializeComponent();
+    }
+}
